79b7a233937c08dce38c854823bfec6a5e971cc0
satya042 <jenasatyapraksh042@gmail.com>
2023-07-12 14:39:02 +0530

    merge navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,12 +5,9 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
 import UserIcon from '../assets/user_icon.png';
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
@@ -25,55 +22,65 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
-//import { makeStyles, Theme, createStyles } from '@mui/styles';
+import { makeStyles, createStyles } from '@mui/styles';
+import { AiTwotoneThunderbolt } from "react-icons/ai";
+import { BsBook } from "react-icons/bs";
+import { RxAvatar } from "react-icons/rx";
+import { Theme } from '@mui/material';
+import { Link } from 'react-router-dom'
+import '../styles/nav.css'
+import { NavbarLink } from "../styles/nav-button-style";
 
-const pages = ['About', 'Blog', 'Links'];
-type Anchor = 'top';
+
 
 interface NavItem {
-  label: string;
+  icon: React.ReactNode;
+  name: string;
   link: string;
 }
 
-const navItems: NavItem[] = [
-  { label: 'Home', link: '/' },
-  { label: 'About', link: '/about' },
-  { label: 'Services', link: '/services' },
-  { label: 'Contact', link: '/contact' },
-];
 
-// const useStyles = makeStyles((theme: Theme) =>
-//   createStyles({
-//     appBar: {
-//       zIndex: theme.zIndex.drawer + 1,
-//     },
-//     menuButton: {
-//       marginRight: theme.spacing(2),
-//     },
-//     drawer: {
-//       width: 240,
-//     },
-//     drawerPaper: {
-//       width: 240,
-//     },
-//   })
-// );
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    drawer: {
+      width: 240,
+    },
+    drawerPaper: {
+      width: 240,
+    },
+  })
+);
 
-function Navbar() {
-  
-  const [open,isOpen] = React.useState(false);
+const Navbar: React.FC = () => {
+
+  const navItems: NavItem[] = [
+    { name: "About", icon: <RxAvatar />, link: "/About" },
+    { name: 'Tech Stack', icon: <AiTwotoneThunderbolt />, link: '/TechStack' },
+    { name: 'Open Source', icon: <BsBook />, link: '/OpenSource' },
+  ];
   const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
-  
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  
-  //const [state, setState] = React.useState({top: false,});
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
   };
+  const classes = useStyles();
+  // const [open, isOpen] = React.useState(false);
+  //const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  //const [state, setState] = React.useState({top: false,});
+  // const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+  //   setAnchorElNav(event.currentTarget);
+  // };
+
+  // const handleCloseNavMenu = () => {
+  //   setAnchorElNav(null);
+  // };
 
   // const toggleDrawer = () =>
   // (event: React.KeyboardEvent | React.MouseEvent) => {
@@ -88,18 +95,18 @@ function Navbar() {
   //   // setState({ ...state, [anchor]: open });
   // };
 
-  const toggleDrawer = () => {
-    setIsDrawerOpen(!isDrawerOpen);
-  };
+
 
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
 
   return (
-      <AppBar position="static" sx={{
-          backgroundColor:'background.default',
-          boxShadow: 5,
+    <AppBar position="sticky" component="nav" className={classes.appBar}
+      sx={{
+        backgroundColor: 'navbar.default',
+        boxShadow: 5,
+        zIndex: 1250,
       }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
@@ -110,83 +117,62 @@ function Navbar() {
               aria-controls="menu-appbar"
               onClick={toggleDrawer}
               color="inherit"
-             
             >
-             {isDrawerOpen ? <AiOutlineClose /> : <GiHamburgerMenu />} 
+              {isDrawerOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
             </IconButton>
-
-            {/* 
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu> */}
-    
-
-
             <Drawer
               anchor="top"
               variant="temporary"
               open={isDrawerOpen}
-              onClose={handleCloseNavMenu}
-              sx={{width:10,marginTop:6}}
+              onClose={toggleDrawer}
+              sx={{ width: 10, zIndex: 1190 }}
+              BackdropProps={{ invisible: true }}
             >
-              <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                  <ListItem key={text} disablePadding>
+              <List sx={{ backgroundColor: 'primary.main' }}>
+                {navItems.map((text, index) => (
+                  <ListItem key={index} disablePadding>
                     <ListItemButton>
-                      <ListItemText primary={text} />
+                      <Link style={{ textDecoration: "none", color: 'white' }} to={text.link}>
+                        <ListItemIcon>{text.icon}</ListItemIcon>
+                        <ListItemText primary={text.name} onClick={toggleDrawer} />
+                      </Link>
                     </ListItemButton>
                   </ListItem>
                 ))}
               </List>
             </Drawer>
-     
-
-            
           </Box>
-          <Avatar variant="circular" src={UserIcon}></Avatar>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >   
-          </Typography>
+          <Link to={'/'}>
+            <Avatar variant="circular" src={UserIcon}></Avatar>
+          </Link>
+          <Typography sx={{mr: 2,display: { xs: 'flex', md: 'none' },flexGrow: 1,}}> </Typography>
           <Box pl={2} sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
+            {navItems.map((page, index) => (
               <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{my: 1, px:2, color: "white", display: "block", textTransform: "none",fontSize:"22px"  }}
+                key={index}
+                size='medium'
+                sx={{
+                  my: 1, px: 2, mx: 0.3, textTransform: "none", fontSize: "17px", borderRadius: "10px", variant: "outlined", color: "navbar.main",
+                  ":hover": { bgcolor: "navbar.hover", color: "#7c98fc" }
+                }}
+                startIcon={page.icon}
               >
-                {page}
+                {/* <Link  style={{textDecoration:"none",color:theme.palette.mode==="dark" ?"#ffffff":"#2ba3ff" }} to={page.link}>{" "}{page.name}</Link> */}
+                <NavbarLink style={{ color: theme.palette.mode === "dark" ? "#ffffff" : "#000000" }} to={page.link}>{page.name}</NavbarLink>
               </Button>
             ))}
           </Box>
-
           <Box sx={{ flexGrow: 0 }}>
-          <IconButton>
-               <GitHubIcon />
-             </IconButton>
-             <IconButton onClick={colorMode.toggleColorMode}>
-               {theme.palette.mode === "dark" ? (
-                 <DarkModeOutlinedIcon />
-               ) : (
-                 <LightModeOutlinedIcon />
-               )}
-             </IconButton>     
+            <IconButton>
+              <GitHubIcon />
+            </IconButton>
+            <IconButton onClick={colorMode.toggleColorMode}>
+              {theme.palette.mode === "dark" ? (
+                <DarkModeOutlinedIcon />
+              ) : (
+                <LightModeOutlinedIcon />
+              )}
+            </IconButton>
           </Box>
         </Toolbar>
       </Container>
@@ -195,86 +181,3 @@ function Navbar() {
 }
 export default Navbar;
 
-
-
-
-// import React, { useState } from 'react';
-// //import { makeStyles, Theme, createStyles } from '@mui/styles';
-// import { AppBar, Toolbar, IconButton, Drawer, List, ListItem, ListItemText } from '@mui/material';
-// import { Menu as MenuIcon, Close as CloseIcon } from '@mui/icons-material';
-
-// // const useStyles = makeStyles((theme: Theme) =>
-// //   createStyles({
-// //     appBar: {
-// //       zIndex: theme.zIndex.drawer + 1,
-// //     },
-// //     menuButton: {
-// //       marginRight: theme.spacing(2),
-// //     },
-// //     drawer: {
-// //       width: 240,
-// //     },
-// //     drawerPaper: {
-// //       width: 240,
-// //     },
-// //   })
-// // );
-
-// interface NavItem {
-//   label: string;
-//   link: string;
-// }
-
-// const Navbar: React.FC = () => {
-//   //const classes = useStyles();
-//   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-
-  // const toggleDrawer = () => {
-  //   setIsDrawerOpen(!isDrawerOpen);
-  // };
-
-//   const navItems: NavItem[] = [
-//     { label: 'Home', link: '/' },
-//     { label: 'About', link: '/about' },
-//     { label: 'Services', link: '/services' },
-//     { label: 'Contact', link: '/contact' },
-//   ];
-
-//   return (
-//     <div>
-//       <AppBar position="fixed" >
-//         <Toolbar>
-//           <IconButton
-//             edge="start"
-//             color="inherit"
-//             aria-label="menu"
-            
-//             onClick={toggleDrawer}
-//           >
-//             {isDrawerOpen ? <CloseIcon /> : <MenuIcon />}
-//           </IconButton>
-//           {/* Add your logo here */}
-//         </Toolbar>
-//       </AppBar>
-//       <Drawer
-//         sx={{width:240}}
-//         variant="persistent"
-//         anchor="top"
-//         open={isDrawerOpen}
-      
-//       >
-//         <List>
-//           {navItems.map((item, index) => (
-//             <ListItem button key={index}>
-//               <ListItemText primary={item.label} />
-//             </ListItem>
-//           ))}
-//         </List>
-//       </Drawer>
-//       <Toolbar /> {/* Add space for the fixed AppBar */}
-//       {/* Your page content goes here */}
-//     </div>
-//   );
-// };
-
-// export default Navbar;
